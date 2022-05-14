540eaaa36dc9931e40304646fed4fa695478e139
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-05-14 17:01:40 +0530

    Add type casting

--- basics/Variables/main.py
+++ basics/Variables/main.py
@@ -16,3 +16,24 @@ color = input("Your color ? ")
 print(name + " is "+ age +" years old. He likes "+color)
 
 # String
+
+course = "Python's course for beginners"
+another = course[:]
+course_new = 'Python for "beginners"'
+email = '''
+Hi Madusanka,
+How are you. Hope we can meet tomorrow.
+Thank you.
+'''
+
+print(course)
+print(another)
+print(course_new)
+print(email)
+print(course[0])
+print(course[-1])
+print(course[0:])
+
+# Including first index and excluding last index
+print(course[1:3])
+print(course[:5])
