48e38e13bd96b3c64d3f329a5d35e4461a169cfb
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-05-21 08:11:39 +0530

    Add Jupyter notebook exercise

--- basics/File/sololearn.py
+++ basics/File/sololearn.py
@@ -2,3 +2,11 @@ file = open("text.txt", "r")
 print(file.read())
 file.close()
 
+# Valuable functions
+numbers = [55, 67, 89, 90, 99, 89]
+
+if all([i > 50 for i in numbers]):
+    print('All are greater than 50')
+
+print([i > 50 for i in numbers])
+
